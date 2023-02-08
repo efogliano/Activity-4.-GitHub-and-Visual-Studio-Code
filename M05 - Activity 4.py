8f74b0bb43fa7f064452b38abd63fe719645c0f8
Eloy <a1603142@institutmontilivi.cat>
2023-02-08 18:26:27 +0100

    Versió amb preguntes

--- M05 - Activity 4.py
+++ M05 - Activity 4.py
@@ -1,6 +1,32 @@
 #Eloy
 print("M05 - ACTIVITY 4")
 
+#Mostrar Figura1
+try:
+    answer = int (input("How many triangles are there in the following figure?"))
+    if answer == 24: 
+        valid=True
+except:
+    print("Incorrect format, a number must be entered.")
+
+if valid:
+    print("Congratulations!")
+else:
+    print("Is wrong...")
+
+#Mostrar Figura2
+try:
+    answer = int (input("How many squares are there in the following figure?"))
+    if answer == 20: 
+        valid=True
+except:
+    print("Incorrect format, a number must be entered.")
+
+if valid:
+    print("Congratulations!")
+else:
+    print("Is wrong...")
+
 #Arnau
 
 #Jaume
