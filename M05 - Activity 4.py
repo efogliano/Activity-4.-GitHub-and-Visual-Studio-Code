947346ccc1c5762f2246f6faba1bccf33a3585e6
abonavia <a2208025@institutmontilivi.cat>
2023-02-13 17:50:32 +0100

    Arnau's Basic structure

--- M05 - Activity 4.py
+++ M05 - Activity 4.py
@@ -3,10 +3,11 @@
 #Arnau
 def triangle():
     from tkinter import Tk, Canvas, Button, mainloop
-    #parametres d'alçada i amplada per fer finestra variable
-    altura=600
+    #parametres d'altura i amplada per fer finestra variable
+    print("How big do you want the windows?")
+    altura=500
     amplada=500
-    # mesCares = percentatgeCares*altura/100 → Calcula amb percentatge la coordenada, útil per responsive
+    # mesCares = percentatgeCares*altura/100 -> Calcula amb percentatge la coordenada, util per responsive
 
 
 
@@ -14,13 +15,21 @@ def triangle():
     canvas = Canvas(finestra, width=amplada, height=altura, background="grey")
 
     #Text
-    canvas.create_text(amplada*0.3,altura-mesCreus-altura*0.01, text="Creus")
+    canvas.create_text(amplada*0.5,altura*0.05, text="How many Triangles")
 
 
     #Linies
-    canvas.create_line(amplada*0.2,altura*0.2,amplada*0.4,altura*0.4)
+    canvas.create_line(amplada*0.1,altura*0.9,amplada*0.5,altura*0.1)
+    canvas.create_line(amplada*0.5,altura*0.1,amplada*0.9,altura*0.9)
+    canvas.create_line(amplada*0.1,altura*0.9,amplada*0.9,altura*0.9)
 
 
 
     canvas.pack()
+    mainloop()
+    print("oi")
 #Jaume
+
+#Main
+print("prova")
+triangle()
