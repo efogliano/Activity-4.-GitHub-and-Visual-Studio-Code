184d70e7ef43e1e149407f3990c5fc0f3924833f
abonavia <a2208025@institutmontilivi.cat>
2023-02-14 18:22:38 +0100

    Arnau final version

--- M05 - Activity 4.py
+++ M05 - Activity 4.py
@@ -1,34 +1,48 @@
 #Eloy
 
 #Arnau
-def triangle():
+#Function to get a positive value
+def GetPositiveValue():
+    pixels=0
+    correct=False
+    while not correct:
+        try:
+            pixels=int(input())
+            if pixels>0:
+                correct=True
+            else: print("Enter a valid number bigger than 0")
+        except:
+            print("Enter a valid number bigger than 0")
+    return pixels
+
+#Function of the Triangle Problem
+def Triangle():
     from tkinter import Tk, Canvas, Button, mainloop
-    #parametres d'altura i amplada per fer finestra variable
-    print("How big do you want the windows?")
-    altura=500
-    amplada=500
+    #parametres of width and height for the drawing window
+    print("How big do you want the emerging window?")
+    print("The width in pixels")
+    altura=GetPositiveValue()
+    print("The height in pixels")
+    amplada=GetPositiveValue()
     # mesCares = percentatgeCares*altura/100 -> Calcula amb percentatge la coordenada, util per responsive
 
-
-
-    finestra = Tk() #Finestra Tk Inter per dibuixar a dintre
-    canvas = Canvas(finestra, width=amplada, height=altura, background="grey")
+    window = Tk() #Drawing window of Tk Inter
+    canvas = Canvas(window, width=amplada, height=altura, background="grey")
 
     #Text
-    canvas.create_text(amplada*0.5,altura*0.05, text="How many Triangles")
+    canvas.create_text(amplada*0.5,altura*0.05, text="How many Triangles in total?")
 
-
-    #Basic Linies
+    #Main Triangle Lines
     canvas.create_line(amplada*0.1,altura*0.9,amplada*0.5,altura*0.1)
     canvas.create_line(amplada*0.5,altura*0.1,amplada*0.9,altura*0.9)
     canvas.create_line(amplada*0.1,altura*0.9,amplada*0.9,altura*0.9)
 
-    #Interior Horizontal
+    #Interior Horizontal Lines
     canvas.create_line(amplada*0.4,altura*0.3,amplada*0.6,altura*0.3)
     canvas.create_line(amplada*0.3,altura*0.5,amplada*0.7,altura*0.5)
     canvas.create_line(amplada*0.2,altura*0.7,amplada*0.8,altura*0.7)
 
-    #Interior Vertical
+    #Interior Vertical Lines
     canvas.create_line(amplada*0.35,altura*0.9,amplada*0.5,altura*0.1)
     canvas.create_line(amplada*0.5,altura*0.1,amplada*0.65,altura*0.9)
 
@@ -37,5 +51,4 @@ def triangle():
 #Jaume
 
 #Main
-print("prova")
-triangle()
+Triangle()
